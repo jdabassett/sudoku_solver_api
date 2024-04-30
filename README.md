575267d3f8ad6a6fd8eb1c17db8b639e6af82a6a
jdabassett <jacobbassett@gmail.com>
2024-04-29 20:14:04 -0600

    unit test mostly done and error handling working on view

--- README.md
+++ README.md
@@ -4,7 +4,8 @@ Can run this in the terminal to test the application locally.
 ```bash
 curl -X POST \
   'http://127.0.0.1:8000/api/v1/solve/' \
-  --header 'Accept: image/jpeg' \
-  --form 'puzzle=@/Users/jacobbassett/projects/courses/sudoku/OpenCV-Sudoku-Solver/data/sudokus/2.jpg' \
-  -o solved_2.jpg
+  -H 'Accept: */*' \
+  -H 'User-Agent: Thunder Client (https://www.thunderclient.com)' \
+  -F 'puzzle=@/Users/jacobbassett/projects/courses/sudoku/backend/data/puzzles/1.jpg' \
+  -o solved_29_0.jpg
 ```
--- solver/views.py
+++ solver/views.py
@@ -1,8 +1,7 @@
 from django.http import HttpResponse
 from rest_framework.views import APIView
 
-from .sudoku_solver import (
-    solve_board)
+from .sudoku_solver import solve_board
 from .utilities import (
     biggest_contour,
     convert_file_to_nparray,
@@ -15,74 +14,147 @@ from .utilities import (
     perspective_warp,
     preprocess_image,
     reorder,
-    split_boxes)
-
-
-###########################################################
-test_puzzle = [[2, 1, 0, 0, 6, 0, 9, 0, 0], [0, 0, 0, 0, 0, 9, 1, 0, 0], [4, 0, 9, 3, 1, 0, 0, 5, 8], [0, 0, 1, 0, 0, 5, 0, 4, 0], [9, 0, 4, 0, 3, 0, 8, 0, 5], [0, 5, 0, 2, 0, 0, 6, 0, 0], [3, 8, 0, 0, 4, 0, 5, 0, 6], [0, 0, 6, 7, 0, 0, 0, 0, 2], [0, 0, 7, 0, 8, 0, 3, 0, 9]]
-###########################################################
+    split_boxes,
+)
 
 
 class Sudoku_API(APIView):
-  def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
-    self.model = initialize_prediction_model()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.model = initialize_prediction_model()
 
-  def post(self, request):
-    try:
-       
-      # get image from request
-      if 'puzzle' not in request.FILES:
-              return HttpResponse({"message": "Puzzle file not supplied", "error":"User must supply image of unsolved sudoku puzzle."}, status=400)
-      
-      puzzle = request.FILES['puzzle']
-      
-      # convert image into nparray
-      try:
-          img = convert_file_to_nparray(puzzle)
-      except Exception as e:
-          return HttpResponse({"message": "Failed to convert file to numpy array", "error": str(e)}, status=400)
-      
-      # preprocess image
-      try:
-          img_proc = preprocess_image(img)
-      except Exception as e:
-          return HttpResponse({"message": "Failed to preprocess image", "error": str(e)}, status=400)
-            
+    def post(self, request):
+        try:
+            print("View")
+            # get image from request
+            if "puzzle" not in request.FILES:
+                return HttpResponse(
+                    {
+                        "message": "Puzzle file not supplied",
+                        "error": "User must supply image of unsolved sudoku puzzle.",
+                    },
+                    status=400,
+                )
 
-      # find contours
-      contours = find_contours(img_proc)
+            print("Puzzle")
+            # extract unsolved puzzle from request
+            puzzle = request.FILES["puzzle"]
 
-      # find outer border
-      border = biggest_contour(contours)
-      border = reorder(border)
+            print("Image Type")
+            # reject incorrect file types
+            if not puzzle.name.lower().endswith((".jpg", ".jpeg", ".png")):
+                return HttpResponse(
+                    {
+                        "message": "Invalid file type, image must be a JPEG or PNG file.",
+                        "error": "Only JPEG, PNG, and BMP file types are allowed.",
+                    },
+                    status=400,
+                )
 
-      # # apply perspective shift
-      img_persp = perspective_warp(border, img)
+            print("Convert")
+            try:
+                # convert image into nparray
+                img = convert_file_to_nparray(puzzle)
+            except Exception as e:
+                return HttpResponse(
+                    {
+                        "message": "Failed to convert file to numpy array",
+                        "error": str(e),
+                    },
+                    status=400,
+                )
 
-      # # split puzzle into cells
-      cells = split_boxes(img_persp)
+            print("Process")
+            try:
+                # preprocess image
+                img_proc = preprocess_image(img)
+            except Exception as e:
+                return HttpResponse(
+                    {"message": "Failed to preprocess image.", "error": str(e)},
+                    status=400,
+                )
 
-      # # extract unsolved puzzle
-      # TODO: if predictions cannot be made
-      unsolved, _ = get_prediction(cells, self.model)
+            print("Borders")
+            try:
+                # find contours
+                contours = find_contours(img_proc)
+                # find outer border
+                border = biggest_contour(contours)
+                border = reorder(border)
+            except Exception as e:
+                return HttpResponse(
+                    {"message": "Failed to find borders of puzzle.", "error": str(e)},
+                    status=400,
+                )
 
-      # # solve board
-      # TODO: if solution cannot be made
-      solved = solve_board(test_puzzle)
+            print("Perspective and Spliting")
+            try:
+                # apply perspective shift
+                img_persp = perspective_warp(border, img)
+                # split puzzle into cells
+                cells = split_boxes(img_persp)
+            except Exception as e:
+                return HttpResponse(
+                    {
+                        "message": "Failed to locate each square of the sudoku puzzle.",
+                        "error": str(e),
+                    },
+                    status=400,
+                )
+            print("Make Predictions")
+            try:
+                # extract unsolved puzzle
+                unsolved, _ = get_prediction(cells, self.model)
+            except Exception as e:
+                return HttpResponse(
+                    {
+                        "message": "Failed to make prediction of each square in puzzle.",
+                        "error": str(e),
+                    },
+                    status=400,
+                )
+            print("Solve Board")
+            try:
+                # solve board
+                solved = solve_board(unsolved)
+            except Exception as e:
+                return HttpResponse(
+                    {
+                        "message": "Failed to generate solution to puzzle.",
+                        "error": str(e),
+                    },
+                    status=400,
+                )
 
-      # overlay solution to input image
-      # TODO: solution cannot be overlayed or converted to return image
-      img_mask = display_numbers(unsolved, solved, img.shape[:-1])
-      img_ans = overlay_solution(img, img_mask, border, img.shape[:-1])
+            print("Overlay Solution")
+            try:
+                # overlay solution to input image
+                img_mask = display_numbers(unsolved, solved, img.shape[:-1])
+                img_ans = overlay_solution(img, img_mask, border, img.shape[:-1])
+            except Exception as e:
+                return HttpResponse(
+                    {
+                        "message": "Failed overlay solution onto puzzle.",
+                        "error": str(e),
+                    },
+                    status=400,
+                )
 
-      # convert from np.ndarray to jpg
-      try:
-          img_jpg = convert_nparray_to_jpg(img_ans)
-      except Exception as e:
-          return HttpResponse({"message": "Failed to convert image to JPEG", "error": str(e)}, status=400)
-            
-      return HttpResponse(img_jpg, content_type="image/jpeg", status=200)
-    except Exception as e:
-        return HttpResponse({"message": "An unexpected error occurred", "error": str(e)}, status=500)
+            print("Convert Back")
+            try:
+                # convert from np.ndarray to jpg
+                img_jpg = convert_nparray_to_jpg(img_ans)
+            except Exception as e:
+                return HttpResponse(
+                    {
+                        "message": "Failed to convert solved puzzle into JPEG.",
+                        "error": str(e),
+                    },
+                    status=400,
+                )
 
+            return HttpResponse(img_jpg, content_type="image/jpeg", status=200)
+        except Exception as e:
+            return HttpResponse(
+                {"message": "An unexpected error occurred", "error": str(e)}, status=500
+            )
